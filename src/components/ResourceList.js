cd38c29ebae0913e6966c6213a7d3303c87c6c3e
Mike Wong <wongmdesign@gmail.com>
2019-04-02 11:23:46 -0400

    code resuse with hook for user after refactor

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,31 +1,9 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React from 'react'
+import useResources from './useResources'
 
 // pass resource prop into argument to stay dry
 const ResourceList = ({ resource }) => {
-  // useState hook to replicate this.state and this.setState
-  const [resources, setResources] = useState([])
-
-  const fetchResource = async resource => {
-    const res = await axios(`https://jsonplaceholder.typicode.com/${resource}`)
-    // same as setState of the resource prop to the new state
-    setResources(res.data)
-  }
-
-  // useEffect hook to utilizes lifecycle methods CDU and CDM
-  // the resource that gets passed in is the prop from above which determines if the function gets invoked again or not with the prop
-  // Notes: no array will constant re-fetch spam,
-  // Notes: empty array = CDM
-  // Notes: array with exact same value(s) = CDM. 2nd time it will not re-fetch
-  // Notes: array with changing values = CDM & CDU to re-fetch
-  // Notes: array with exact same object = CDM & CDU due to nature of objects being different references in memory
-  useEffect(
-    () => {
-      fetchResource(resource)
-    },
-    [resource]
-  )
-
+  const resources = useResources(resource)
   return (
     <ul>
       {resources.map(record => (
---
+++ src/components/useResources.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const useResources = resource => {
+  // useState hook to replicate this.state and this.setState
+  const [resources, setResources] = useState([])
+
+  const fetchResource = async resource => {
+    const res = await axios(`https://jsonplaceholder.typicode.com/${resource}`)
+    // same as setState of the resource prop to the new state
+    setResources(res.data)
+  }
+
+  // useEffect hook to utilizes lifecycle methods CDU and CDM
+  // the resource that gets passed in is the prop from above which determines if the function gets invoked again or not with the prop
+  // Notes: no array will constant re-fetch spam,
+  // Notes: empty array = CDM
+  // Notes: array with exact same value(s) = CDM. 2nd time it will not re-fetch
+  // Notes: array with changing values = CDM & CDU to re-fetch
+  // Notes: array with exact same object = CDM & CDU due to nature of objects being different references in memory
+  useEffect(
+    () => {
+      fetchResource(resource)
+    },
+    [resource]
+  )
+  return resources
+}
+
+export default useResources
