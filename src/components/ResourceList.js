9f044cb6a01a0bc927412823bdd4cd861495a06a
Mike Wong <wongmdesign@gmail.com>
2019-04-02 10:08:55 -0400

    component did updated added for separate fetch

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,7 +1,28 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class ResourceList extends Component {
+  state = { resources: [] }
+
+  async componentDidMount () {
+    const res = await axios(
+      `https://jsonplaceholder.typicode.com/${this.props.resource}`
+    )
+    this.setState({ resources: res.data })
+  }
+
+  // Lifecycle method gets called anytime our component re-renders either because of a parent component or whenever we call setSTate inside current component
+  async componentDidUpdate (prevProps) {
+    // if the previous props resource is different from current (based on click change), do another fetch to specified api to re-render component
+    if (prevProps.resource !== this.props.resource) {
+      const res = await axios(
+        `https://jsonplaceholder.typicode.com/${this.props.resource}`
+      )
+      this.setState({ resources: res.data })
+    }
+  }
+
   render () {
-    return <div>{this.props.resource}</div>
+    return <div>{this.state.resources.length}</div>
   }
 }
